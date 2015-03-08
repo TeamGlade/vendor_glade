d75d6c7f860060509292212b255871b25450afc5
Jackeagle <jackeagle102@gmail.com>
2015-03-08 04:18:34 -0400

    Disable ADB Authentication On Boot Remove CM Setup Wizard.
    Change-Id: Ic6a6783b69e304e8f48c209c15443e7d9ecfe7b0
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -60,7 +60,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
 endif
 
 # Copy over the changelog to the device
@@ -139,8 +139,7 @@ PRODUCT_PACKAGES += \
     Eleven \
     LockClock \
     CMHome \
-    DashClock \
-    CyanogenSetupWizard
+    DashClock 
 
 # Glade Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
