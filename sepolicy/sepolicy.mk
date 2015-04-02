594122761c5c4237e8b6bb2daec0ec0dfe514183
Jackeagle <jackeagle102@gmail.com>
2015-04-02 10:47:35 -0400

    Add More Sepolicies Based On Commit : https://github.com/F-AOSP/platform_vendor_aosp/commit/d3359a0263fe2a0020173f98a055139d8182d5a4 Credits : CyanogenMod , F-AOSP
    Change-Id: I35a9feff734eb47569ae76bda189f7558ae0d30b
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -7,7 +7,18 @@ BOARD_SEPOLICY_DIRS += \
     vendor/glade/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-service_contexts \
-system.te
-  
-
+    service_contexts \
+    system.te
+    file.te \
+    file_contexts \
+    genfs_contexts \
+    seapp_contexts \
+    service_contexts \
+    auditd.te \
+    installd.te \
+    netd.te \
+    sysinit.te \
+    system.te \
+    ueventd.te \
+    vold.te \
+    mac_permissions.xml
